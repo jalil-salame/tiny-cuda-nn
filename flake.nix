dc7221b810800101fba87f04d0c0265ccbe0f5b4
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-02-27 00:11:06 +0100

    feat: Export overlay

--- flake.nix
+++ flake.nix
@@ -5,62 +5,61 @@
     let
       supportedSystems = [ "x86_64-linux" ];
       config.allowUnfree = true;
-      overlays = [
-        (final: prev: {
-          tiny-cuda-nn = prev.tiny-cuda-nn.overrideAttrs (old:
-            let
-              cuda-common-redist = with final.cudaPackages; [
-                cuda_cudart.dev # cuda_runtime.h
-                cuda_cudart.lib
-                cuda_cccl.dev # <nv/target>
-                libcublas.dev # cublas_v2.h
-                libcublas.lib
-                libcusolver.dev # cusolverDn.h
-                libcusolver.lib
-                libcusparse.dev # cusparse.h
-                libcusparse.lib
-              ];
-              cuda-common-redist' = with final.cudaPackages; [
-                cuda_cudart.dev # cuda_runtime.h
-                cuda_cudart.lib
-                cuda_cccl.dev # <nv/target>
-                libcublas.dev # cublas_v2.h
-                libcublas.lib
-                libcusolver.dev # cusolverDn.h
-                libcusolver.lib
-                libcusparse.dev # cusparse.h
-                libcusparse.lib
-                cudatoolkit
-              ];
-              cuda-native-redist = final.symlinkJoin {
-                name = "cuda-redist";
-                paths = with final.cudaPackages;
-                  [ cuda_nvcc ]
-                  ++ cuda-common-redist;
-              };
-              cuda-native-redist' = final.symlinkJoin {
-                name = "cuda-redist";
-                paths = with final.cudaPackages;
-                  [ cuda_nvcc ]
-                  ++ cuda-common-redist';
-              };
-              cuda-redist = final.symlinkJoin {
-                name = "cuda-redist";
-                paths = cuda-common-redist;
-              };
-              cuda-redist' = final.symlinkJoin {
-                name = "cuda-redist";
-                paths = cuda-common-redist';
-              };
-              inherit (nixpkgs.lib) remove;
-              nativeBuildInputs = [ cuda-native-redist' ] ++ (remove cuda-native-redist old.nativeBuildInputs);
-              buildInputs = [ cuda-redist' ] ++ (remove cuda-redist old.buildInputs);
-            in
-            {
-              inherit nativeBuildInputs buildInputs;
-            });
-        })
-      ];
+      tiny-cuda-nn-fix = final: prev: {
+        tiny-cuda-nn = prev.tiny-cuda-nn.overrideAttrs (old:
+          let
+            cuda-common-redist = with final.cudaPackages; [
+              cuda_cudart.dev # cuda_runtime.h
+              cuda_cudart.lib
+              cuda_cccl.dev # <nv/target>
+              libcublas.dev # cublas_v2.h
+              libcublas.lib
+              libcusolver.dev # cusolverDn.h
+              libcusolver.lib
+              libcusparse.dev # cusparse.h
+              libcusparse.lib
+            ];
+            cuda-common-redist' = with final.cudaPackages; [
+              cuda_cudart.dev # cuda_runtime.h
+              cuda_cudart.lib
+              cuda_cccl.dev # <nv/target>
+              libcublas.dev # cublas_v2.h
+              libcublas.lib
+              libcusolver.dev # cusolverDn.h
+              libcusolver.lib
+              libcusparse.dev # cusparse.h
+              libcusparse.lib
+              cudatoolkit
+            ];
+            cuda-native-redist = final.symlinkJoin {
+              name = "cuda-redist";
+              paths = with final.cudaPackages;
+                [ cuda_nvcc ]
+                ++ cuda-common-redist;
+            };
+            cuda-native-redist' = final.symlinkJoin {
+              name = "cuda-redist";
+              paths = with final.cudaPackages;
+                [ cuda_nvcc ]
+                ++ cuda-common-redist';
+            };
+            cuda-redist = final.symlinkJoin {
+              name = "cuda-redist";
+              paths = cuda-common-redist;
+            };
+            cuda-redist' = final.symlinkJoin {
+              name = "cuda-redist";
+              paths = cuda-common-redist';
+            };
+            inherit (nixpkgs.lib) remove;
+            nativeBuildInputs = [ cuda-native-redist' ] ++ (remove cuda-native-redist old.nativeBuildInputs);
+            buildInputs = [ cuda-redist' ] ++ (remove cuda-redist old.buildInputs);
+          in
+          {
+            inherit nativeBuildInputs buildInputs;
+          });
+      };
+      overlays = [ tiny-cuda-nn-fix ];
       forEachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         inherit system;
         pkgs = import nixpkgs { inherit system config overlays; };
@@ -68,6 +67,7 @@
     in
     {
       formatter = forEachSystem ({ pkgs, ... }: pkgs.nixpkgs-fmt);
+      overlays = { inherit tiny-cuda-nn-fix; };
       packages = forEachSystem
         ({ pkgs, ... }: {
           default = pkgs.tiny-cuda-nn;
